35b2ba97fa821d92d1fed97f042abf6f9b3c603b
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-06-05 12:41:28 -0300

    Inserção descrição, data e imagem

--- form.php
+++ form.php
@@ -1,7 +1,13 @@
 <div class="form">
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <label for="nome-tarefa">Tarefa:</label>
         <input type="text" name="nome-tarefa" id="nome-tarefa" placeholder="Nome da Tarefa">
+        <label for="nome-tarefa">Descrição:</label>
+        <input type="text" name="descricao-tarefa" id="descricao-tarefa" placeholder="Descrição da Tarefa">
+        <label for="data-tarefa">Data:</label>
+        <input type="date" name="data-tarefa" id="data-tarefa">
+        <label for="imagem-tarefa">Imagem:</label>
+        <input type="file" name="imagem-tarefa" id="imagem-tarefa">
         <input type="submit" value="Adicionar" class="button">
     </form>
 
