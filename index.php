49f9c4d2f95722d6cd88f41df0ba0ebea4d82546
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-06-05 12:42:28 -0300

    Refatoração: inserção atualização da sessão detalhe, data, img.

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <?php 
 session_start();
 
-if (!isset($_SESSION['tarefas'])) {
+if (!isset($_SESSION['tarefas']) || !is_array($_SESSION['tarefas'])) {
     $_SESSION['tarefas'] = array();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['nome-tarefa'])) {
+
+    if (isset($_POST['nome-tarefa']) && isset($_POST['descricao-tarefa']) && isset($_POST['data-tarefa'])) {
+
         if ($_POST['nome-tarefa'] != "") {
-            array_push($_SESSION['tarefas'], $_POST['nome-tarefa']);
+
+            if ( isset($_FILES['imagem-tarefa']) ) {
+                  $extensao = strtolower( substr($_FILES['imagem-tarefa']['name'], -4) );
+                  $nome_arquivo = md5( date('Y.m.d.H.i.s') ) . $extensao;
+                  $diretorio = 'uploads/';
+                  
+                  move_uploaded_file($_FILES['imagem-tarefa']['tmp_name'], $diretorio.$nome_arquivo);
+            }
+
+            $dados = [
+                'nome-tarefa' => $_POST['nome-tarefa'],
+                'descricao-tarefa' => $_POST['descricao-tarefa'],
+                'data-tarefa' => $_POST['data-tarefa'],
+                'imagem-tarefa' => $nome_arquivo
+            ];
+
+            array_push($_SESSION['tarefas'], $dados);
             unset($_POST['nome-tarefa']);
+            unset($_POST['descricao-tarefa']);
+            unset($_POST['data-tarefa']);
+            
         } else {
             $_SESSION['mensagem'] = "Preencha o nome da tarefa!";
         }
